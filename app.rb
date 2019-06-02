f843026d3f2f642f431da0a3c56cf0433e58690b
Nicholas Orochena <nicholas.orochena@gmail.com>
2019-06-02 00:21:45 -0400

    complted lab

--- app.rb
+++ app.rb
@@ -1,9 +1,9 @@
+# frozen_string_literal: true
+
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-
-  get '/' do 
-    "Welcome to your app!!!! "
+  get '/' do
+    'Welcome to shotgun app'
   end
-
 end
